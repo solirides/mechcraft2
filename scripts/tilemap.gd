075847f862248fba94dcdd3bc4ff3f3edf9dfb82
corrugatedbread <83032760+solirides@users.noreply.github.com>
2023-10-31 21:52:43 -0700

    e

---
+++ scripts/tilemap.gd
@@ -0,0 +1,21 @@
+extends TileMap
+
+
+
+@export var world = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("left_click"):
+		var p = event.position
+		print(p);
+
+
