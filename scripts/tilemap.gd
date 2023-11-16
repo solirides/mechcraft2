e2338bd2d96e35897828f38714613f6933c1fe00
corrugatedbread <83032760+solirides@users.noreply.github.com>
2023-11-15 23:58:15 -0800

    procedural tileset

--- scripts/tilemap.gd
+++ scripts/tilemap.gd
@@ -9,18 +9,24 @@ extends TileMap
 var world_tiles = []
 var world_tiledata = []
 var chunk_size
+var chunk_area
 
 const sides = [Vector2i.UP, Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT]
 
 var selected_tile = 1
 var tile_rotation = 0
 
-
+var tileset = TileSet.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	print(json.json)
+	make_tileset_exist(tileset)
+	ResourceSaver.save(tileset, "res://generated_tileset.tres")
+	self.tileset = tileset
+	
 	chunk_size = json.json["chunk_size"]
+	chunk_area = pow(chunk_size, 2)
 	world_tiles = load_tiles()
 	world_tiledata = load_tiledata()
 	
@@ -52,16 +58,26 @@ func _input(event):
 		detect_conveyors_lines()
 
 func detect_world_tiles():
-	var cells = self.get_used_cells(0)
+#	var cells = self.get_used_cells(0)
+	var d = [chunk_size, 1, -chunk_size, -1]
 	
-	for cell in cells:
-		match self.get_cell_source_id(0, cell):
+	for cell in chunk_area:
+#		print(world_tiles[0][cell])
+		match int(world_tiles[0][cell]):
 			1:
 				print("1")
 				
 				var current_cell = cell
 				var loop = true
-				detect_connections(current_cell)
+				var line: Array[int] = []
+				for b in range(4):
+#					var neighbor = self.get_cell_source_id(0, cell + sides[b])
+					var idk = cell + d[b]
+					if idk >= 0 and idk <= chunk_area:
+						var neighbor = world_tiles[0][cell]
+						if neighbor == 1:
+								print("found")
+								line.append(neighbor)
 				
 
 
@@ -79,7 +95,7 @@ func detect_connections(current_cell:Vector2i):
 
 func set_tilemap(world_tiles, world_tiledata):
 	for c in len(world_tiles):
-		for i in pow(chunk_size, 2):
+		for i in chunk_area:
 			print(world_tiles[c][i])
 			self.set_cell(0, \
 				Vector2i(fmod(i, chunk_size), floor(i / chunk_size)), world_tiles[c][i], \
@@ -93,7 +109,7 @@ func load_tiles():
 	print("loading tiles")
 	for i in len(json.json["chunks"]):
 #		print(json.json["chunks"][i]["tiles"])
-		world.append(json.json["chunks"][i]["tiles"])
+		world.append(PackedInt32Array(json.json["chunks"][i]["tiles"]))
 #		print(world[0])
 	
 	return world
@@ -110,3 +126,44 @@ func _on_selection_changed(selected_tile, tile_rotation):
 	self.selected_tile = selected_tile
 	self.tile_rotation = tile_rotation
 	
+func make_tileset_exist(ts: TileSet):
+	var id = 0
+	var path = "res://assets/tiles/"
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			elif file_name.ends_with(".png"):
+				print("Found file: " + file_name)
+				print(path + file_name)
+				var image = Image.load_from_file(path + file_name)
+				var source = TileSetAtlasSource.new()
+				source.texture = ImageTexture.create_from_image(image)
+				source.create_tile(Vector2i(0,0))
+				ts.add_source(source)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	
+
+#	ts.tile_set_name()
+#	ts.tile_set_texture(id, ...)
+#	ts.tile_set_region(id, ...)
+
+func dir_contents(path):
+	var dir = DirAccess.open("res://assets/tiles")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
